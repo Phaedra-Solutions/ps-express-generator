9525b547c8942c9e31a87eb49718b9255d6fc24b
mustafasheikh1 <mustafaahmed726@gmail.com>
2021-05-09 07:03:24 +0500

    chnages

--- content/routes/default/routes-config.js
+++ content/routes/default/routes-config.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-router.post('', controller.default);
+router.get('', controller.default);
 
 module.exports = router;
